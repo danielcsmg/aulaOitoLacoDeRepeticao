625443b27f92e1536788a6897e07f97b20bad0cc
danielcsmg <daniel.csouzamg@gmail.com>
2022-03-31 11:49:06 -0300

    Atualização do algoritmo do exercício Um

--- src/exercicioUm.java
+++ src/exercicioUm.java
@@ -1,13 +1,17 @@
 public class exercicioUm {
     public static void main(String[] args) {
-        for(int i = 0; i <= 100; i=i+2){
-            System.out.println(i);
-        }
-
+        System.out.println("Segunda forma de apresentar os números pares de um a cem.");
+        
         for(int i = 0; i <= 100; i++){
             if(i % 2 == 0){
                 System.out.println(i);
             }
+        }
+
+        System.out.println("Segunda forma de apresentar os números pares de um a cem.");
+        
+        for(int i = 0; i <= 100; i=i+2){
+            System.out.println(i);
         }
     }
 }
