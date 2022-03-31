e934ab7f1e82c4293324597aff4f716eeb1410b3
danielcsmg <daniel.csouzamg@gmail.com>
2022-03-31 11:46:16 -0300

    Segunda forma de fazer o exercício um

--- src/exercicioUm.java
+++ src/exercicioUm.java
@@ -3,5 +3,11 @@ public class exercicioUm {
         for(int i = 0; i <= 100; i=i+2){
             System.out.println(i);
         }
+
+        for(int i = 0; i <= 100; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
     }
 }
